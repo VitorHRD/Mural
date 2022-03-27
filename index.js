893718bd6671eb77cd104f41ab481b4f66cdd074
VitorHRD <vrdev435@gmail.com>
2022-03-27 13:59:38 -0300

    AddHeroku

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require('path')
 const apiRoute = require("./rotes/api")
+const cors =  require("cors")
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors())
 
 app.use("/api",apiRoute);
 
--- rotes/api.js
+++ rotes/api.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const  posts = require('../model/posts');
 const cors = require('cors');
-const { use } = require("express/lib/application");
 
 
 
